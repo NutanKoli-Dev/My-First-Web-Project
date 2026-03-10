2bd632b51e11aefc2e29fde0bb101edc50ff0a42
NutanKoli-Dev <255527076+NutanKoli-Dev@users.noreply.github.com>
2026-03-10 20:54:43 +0530

    Delete My-Neo-Buddy directory

--- My-Neo-Buddy/script.js
+++
@@ -1,9 +0,0 @@
-const darkModeToggle = document.getElementById('darkModeToggle');
-
-darkModeToggle.addEventListener('change', () => {
-    if (darkModeToggle.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-        document.documentElement.removeAttribute('data-theme');
-    }
-});
